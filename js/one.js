e9b5903d081264b79f9e46ab3c59ea328ca53dfc
yangjing <1363907445@qq.com>
2020-04-06 22:31:17 +0800

    提交代码

---
+++ js/one.js
@@ -0,0 +1,26 @@
+// 数组方法
+
+
+const arr1 = [1]
+const arr2 = [2]
+const arr3 = [3]
+
+// concat 
+console.log(arr1.concat(arr2,arr3)) //
+console.log(arr1) //
+console.log(arr2) //
+console.log(arr1.concat(4,5)) //
+console.log(arr1) //
+
+
+//join
+const arrJoin = ['one','two','three']
+console.log(arrJoin.join()) 
+console.log(arrJoin.join('-')) 
+console.log(arrJoin)
+
+
+// pop
+let arrPop = ['one','two','three']
+console.log(arrPop.pop())
+console.log(arrPop)
